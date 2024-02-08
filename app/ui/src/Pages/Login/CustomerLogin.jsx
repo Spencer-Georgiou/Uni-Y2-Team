89ee7d62926ba942a2b6643b8c7b8a5fa3bcb82a
Natasha Fotoohi <zlac232@live.rhul.ac.uk>
2024-02-08 16:54:25 +0000

    added a flowbite form component and modififed it to suite our design

--- app/ui/src/Pages/Login/CustomerLogin.jsx
+++ app/ui/src/Pages/Login/CustomerLogin.jsx
@@ -1,12 +1,22 @@
+import Form_customer from "../../components/Form_customer"
 import Navbar_customer from "../../components/Navbar_customer"
+
 
 
 const CustomerLogin = () => {
     return (
         <div>
             <Navbar_customer />
-
+            <div
+                className="w-full h-screen bg-no-repeat bg-cover bg-center flex justify-center items-center"
+                style={{ backgroundImage: "url('/images/CustomerLoginBackground.png')" }}
+            >
+                <div>
+                    <Form_customer />
+                </div>
+            </div>
         </div>
+
     )
 }
 
