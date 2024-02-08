ad31ed560cbbf89fca752d53543c4a18a5350a59
Natasha Fotoohi <zlac232@live.rhul.ac.uk>
2024-02-08 19:00:12 +0000

    the completed customer login page in react

--- app/ui/src/Pages/Login/CustomerLogin.jsx
+++ app/ui/src/Pages/Login/CustomerLogin.jsx
@@ -8,11 +8,17 @@ const CustomerLogin = () => {
         <div>
             <Navbar_customer />
             <div
-                className="w-full h-screen bg-no-repeat bg-cover bg-center flex justify-center items-center"
+                className="w-full h-screen bg-no-repeat bg-cover bg-center flex justify-center items-center relative"
                 style={{ backgroundImage: "url('/images/CustomerLoginBackground.png')" }}
             >
-                <div>
-                    <Form_customer />
+                <div className="h-[600px] w-[600px] bg-orangy rounded-[25px] flex flex-col justify-between py-8 m-auto">
+                    <div className="flex justify-center ">
+                        <img className="w-40 h-40 mb-10" src="/images/OaxacaLogo.png" alt="picture" />
+                    </div>
+                    <div className="text-2xl text-cherry font-sans font-semibold text-center">Customer Login</div>
+                    <div className="mb-20">
+                        <Form_customer />
+                    </div>
                 </div>
             </div>
         </div>
--- app/ui/src/components/Form_customer.jsx
+++ app/ui/src/components/Form_customer.jsx
@@ -2,14 +2,17 @@ import { Forms } from "flowbite-react"
 
 const Form_customer = () => {
     return (
-        <form className="max-w-sm mx-auto">
+        <form className="max-w-lg mx-auto">
             <div className="mb-5">
-                <input type="text" id="username" className="bg-lemon border border-lemon text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
+                <input type="text" id="username" className="bg-lemon border border-lemon text-black text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
             </div>
             <div className="mb-5">
-                <input type="password" id="password" className="bg-lemon border border-lemon text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required />
+                <input type="password" id="password" className="bg-lemon border border-lemon text-black text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required />
             </div>
-            <button type="submit" className="text-black font-sans font-medium font-semibold bg-lemon hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+            <div className="flex justify-center">
+                <button type="submit" className="text-black font-sans font-medium font-semibold bg-lemon font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+            </div>
+
         </form>
 
     )
