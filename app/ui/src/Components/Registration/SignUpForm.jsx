e8e812ec556f9735ec52934fda68c0b636590db6
Abigail <Abigail.Hunter-Blanco.2022@live.rhul.ac.uk>
2024-02-09 17:01:05 +0000

    Improved structure of user input fields
    Changed the user input fields to better respresent the designs, such as rounding out components, spacing input fields out and adding the logo to the shortened box. Need to research centering components and adding a background image.

--- app/ui/src/Components/Registration/SignUpForm.jsx
+++ app/ui/src/Components/Registration/SignUpForm.jsx
@@ -2,20 +2,21 @@ import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
 
 const SignUpForm = () => {
   return (
-    <form className="flex max-w-md flex-col gap-4">
+    <form className="flex max-w-md flex-col gap-4" class="bg-orange-500 text-center rounded max-w-sm">
+    <img src="images/OaxacaLogo.png" class="h-14 justify-center" alt="Oaxaca Logo" />
       <div>
-        <div className="mb-2 block">
+        <div className="mb-1 block">
           <Label htmlFor="email1" value="Your Email:" />
         </div>
-        <TextInput id="email1" type="email" class="bg-yellow-300 focus:ring-4 focus:outline-amber focus:ring-amber font-medium rounded-lg text-center" placeholder="Email" required />
+        <TextInput id="email1" type="email" class="bg-yellow-300 focus:ring-4 focus:outline-amber focus:ring-amber font-medium rounded-lg text-center mb-2" placeholder="Email" required />
       </div>
       <div>
-        <div className="mb-2 block">
+        <div className="mb-1 block">
           <Label htmlFor="password1" value="Your Password:" />
         </div>
-        <TextInput id="password1" type="Password" class="bg-yellow-300 focus:ring-4 focus:outline-amber focus:ring-amber font-medium rounded-lg text-center" placeholder="Password" required />
+        <TextInput id="password1" type="Password" class="bg-yellow-300 focus:ring-4 focus:outline-amber focus:ring-amber font-medium rounded-lg text-center mb-6" placeholder="Password" required />
       </div>
-      <Button type="submit" class="text-rose bg-yellow-300 focus:ring-4 focus:outline-amber focus:ring-amber font-medium rounded-lg text-sm text-center">Sign Up</Button>
+      <Button type="submit" class="text-rose bg-yellow-300 focus:ring-4 focus:outline-amber focus:ring-amber font-medium rounded-lg text-sm text-center mb-4">Sign Up</Button>
     </form>
   );
 }
