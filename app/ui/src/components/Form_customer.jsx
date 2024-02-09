13e31f566ff16e39eb04960d09a60857e896cfe9
Natasha Fotoohi <zlac232@live.rhul.ac.uk>
2024-02-09 10:46:37 +0000

    updated customer login page completed

--- app/ui/src/components/Form_customer.jsx
+++ app/ui/src/components/Form_customer.jsx
@@ -2,15 +2,15 @@ import { Forms } from "flowbite-react"
 
 const Form_customer = () => {
     return (
-        <form className="max-w-lg mx-auto">
+        <form className="px-6 mx-auto">
             <div className="mb-5">
-                <input type="text" id="username" className="bg-lemon border border-lemon text-black text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required />
+                <b><input type="text" id="username" className=" text-xl text-black font-semibold text-center h-14 bg-lemon border border-lemon rounded-2xl block w-full p-2.5 dark:border-gray-600   dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Username" required /></b>
             </div>
-            <div className="mb-5">
-                <input type="password" id="password" className="bg-lemon border border-lemon text-black text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required />
+            <div className="mb-5 ">
+                <b><input type="text" id="username" className="text-xl text-black font-semibold text-center h-14 bg-lemon border border-lemon rounded-2xl block w-full p-2.5 dark:border-gray-600   dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Password" required /></b>
             </div>
             <div className="flex justify-center">
-                <button type="submit" className="text-black font-sans font-medium font-semibold bg-lemon font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
+                <button type="submit" className=" h-16 text-black font-sans font-semibold bg-lemon rounded-lg text-xl w-40 rounded-2xl px-5 py-2.5 text-center dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
             </div>
 
         </form>
