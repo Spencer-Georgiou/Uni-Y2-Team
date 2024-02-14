a02abbf80ccefc0db8bd46918722a721232941f9
Abigail <Abigail.Hunter-Blanco.2022@live.rhul.ac.uk>
2024-02-14 23:55:33 +0000

    Changed NavBar to use flowbite-react
    Altered original registration navigation bar to flowbite-react, rather than just flowbite. Need to alter to use the same colours as design.

--- app/ui/src/Pages/Registration/index.jsx
+++ app/ui/src/Pages/Registration/index.jsx
@@ -1,13 +1,36 @@
 //Registration Page
-//import { Navbar } from 'flowbite-react'
-import NavBar from "../../Components/Registration/NavBar"
+import { Button, Navbar } from 'flowbite-react'
+//import NavBar from "../../Components/Registration/NavBar"
 import SignUpForm from "../../Components/Registration/SignUpForm"
 
 const Registration= () => {
     return (
         <div>
-            <div class="w-full h-screen bg-fixed bg-center bg-cover" style={{ backgroundImage: "url('/images/CustomerLoginBackground.png')" }}>
-                <NavBar/>
+            <Navbar fluid>
+
+            <Navbar.Collapse>
+                <Navbar.Link href="#" active>
+                Home
+                </Navbar.Link>
+                <Navbar.Link href="#">Menus</Navbar.Link>
+                <Navbar.Link href="#">Order</Navbar.Link>
+                <Navbar.Link href="#">Contact</Navbar.Link>
+            </Navbar.Collapse>
+
+            <Navbar.Brand href="https://flowbite-react.com">
+                <img src="images/OaxacaLogo.png" className="mr-3 h-6 sm:h-9 md:order-1" alt="Oaxaca Logo" />
+            </Navbar.Brand>
+
+            <div className="flex md:order-2">
+                <Button>Register</Button>
+                <Button>Log In</Button>
+                <Navbar.Toggle />
+            </div>
+            
+            </Navbar>
+
+            <div class="w-full h-screen bg-fixed bg-center bg-cover flex justify-center items-center relative" style={{ backgroundImage: "url('/images/CustomerLoginBackground.png')" }}>
+                
                 <div class="h-[500px] w-[400px] bg-amber rounded m-auto">
                     <div class="flex justify-center">
                         <SignUpForm/>
