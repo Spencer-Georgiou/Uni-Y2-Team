66ad3288d38a92ca7d4ccf96cff0169c9a762536
Abigail <Abigail.Hunter-Blanco.2022@live.rhul.ac.uk>
2024-02-15 19:12:48 +0000

    Changed user input fields to use Flowbite React

--- app/ui/src/Pages/Registration/index.jsx
+++ app/ui/src/Pages/Registration/index.jsx
@@ -1,5 +1,5 @@
 //Registration Page
-import { Button, Navbar } from 'flowbite-react'
+import { Button, Navbar, Checkbox, Label, TextInput } from 'flowbite-react'
 //import NavBar from "../../Components/Registration/NavBar"
 import SignUpForm from "../../Components/Registration/SignUpForm"
 
@@ -21,9 +21,9 @@ const Registration= () => {
                 <img src="images/OaxacaLogo.png" className="mr-3 h-6 sm:h-9 md:order-1" alt="Oaxaca Logo" />
             </Navbar.Brand>
 
-            <div className="flex md:order-2">
-                <Button className="text-rose bg-sunflower">Register</Button>
-                <Button className="text-rose bg-sunflower">Log In</Button>
+            <div className="flex space-x-3 md:order-2">
+                <Button pill className="text-rose bg-sunflower">Register</Button>
+                <Button pill className="text-rose bg-sunflower">Log In</Button>
                 <Navbar.Toggle />
             </div>
             
@@ -32,8 +32,25 @@ const Registration= () => {
             <div class="w-full h-screen bg-fixed bg-center bg-cover flex justify-center items-center relative" style={{ backgroundImage: "url('/images/CustomerLoginBackground.png')" }}>
                 
                 <div class="h-[500px] w-[400px] bg-amber rounded m-auto">
+                <img className="w-40 h-40 m-auto" src="/images/OaxacaLogo.png" alt="Oaxaca Logo" />
                     <div class="flex justify-center">
-                        <SignUpForm/>
+                    <form className="flex max-w-md flex-col gap-4">
+                        <div className="text-2xl font-sans font-semibold text-center">Customer Sign Up</div>
+                        <div>
+                            <div className="mb-2 block">
+                            <Label htmlFor="email1" value="Your email" />
+                            </div>
+                            <TextInput id="email1" type="email" placeholder="name@flowbite.com" required />
+                        </div>
+                        <div>
+                            <div className="mb-2 block">
+                            <Label htmlFor="password1" value="Your password" />
+                            </div>
+                            <TextInput id="password1" type="password" required />
+                        </div>
+                        <Button type="submit" className="bg-sunflower text-rose">Submit</Button>
+                        </form>
+    
                     </div>
                 </div>
             </div>
