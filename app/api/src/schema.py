a8e911bcf48344e9737062f893f23d4aca350858
Liu Zhang <Liu.ZHANG.2023@live.rhul.ac.uk>
2024-02-18 23:51:33 +0000

    An allergen with no menuitem returned by a query is serializable.

--- app/api/src/schema.py
+++ app/api/src/schema.py
@@ -1,10 +1,17 @@
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 from marshmallow_sqlalchemy.fields import Nested
 
+from src.model import Allergen
 from src.model import MenuGroup
 
 
 class MenuGroupSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = MenuGroup
+        include_relationships = True
+
+
+class AllergenSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = Allergen
         include_relationships = True
--- app/api/test/test_schema.py
+++ app/api/test/test_schema.py
@@ -1,4 +1,6 @@
+from src.model import Allergen
 from src.model import MenuGroup
+from src.schema import AllergenSchema
 from src.schema import MenuGroupSchema
 
 
@@ -13,3 +15,16 @@ class TestMenuGroupSchema:
         quried_menugroup = db.session.query(MenuGroup).first()
         serialised_menugroup = MenuGroupSchema().dump(quried_menugroup)
         assert serialised_menugroup == expected
+
+
+class TestAllergenSchema:
+    # An allergen with no menuitem returned by a query is serializable.
+    def test_serialise_allergen_no_menuitem(self, db):
+        expected = {'id': 1, 'menuitems': [], 'name': 'Gluten'}
+
+        allergen = Allergen(name="Gluten")
+        db.session.add(allergen)
+        db.session.commit()
+        quiried_allergen = db.session.query(Allergen).first()
+        serialised_allergen = AllergenSchema().dump(quiried_allergen)
+        assert serialised_allergen == expected
