a008790f5e960d61bd72ad6ed5ff590669bf68ea
Liu Zhang <Liu.ZHANG.2023@live.rhul.ac.uk>
2024-02-19 00:12:09 +0000

    A menuitem with no allergen returned by a query is serializable.

--- app/api/src/schema.py
+++ app/api/src/schema.py
@@ -3,15 +3,27 @@ from marshmallow_sqlalchemy.fields import Nested
 
 from src.model import Allergen
 from src.model import MenuGroup
+from src.model import MenuItem
 
 
 class MenuGroupSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = MenuGroup
         include_relationships = True
+        exclude = ["id"]
 
 
 class AllergenSchema(SQLAlchemyAutoSchema):
     class Meta:
         model = Allergen
         include_relationships = True
+        exclude = ["id"]
+
+
+class MenuItemSchema(SQLAlchemyAutoSchema):
+    class Meta:
+        model = MenuItem
+        include_relationships = True
+        exclude = ["id"]
+
+    menugroup = Nested(MenuGroupSchema, exclude=("menuitems",))
--- app/api/test/test_schema.py
+++ app/api/test/test_schema.py
@@ -1,30 +1,54 @@
 from src.model import Allergen
 from src.model import MenuGroup
+from src.model import MenuItem
 from src.schema import AllergenSchema
 from src.schema import MenuGroupSchema
+from src.schema import MenuItemSchema
+from decimal import Decimal
 
 
 class TestMenuGroupSchema:
     # A menu group with no menuitem returned by a query is serializable.
     def test_serialize_menugroup_no_menuitem(self, db):
-        expected = {'category': 'Starter', 'id': 1, 'menuitems': [], 'type': 'Food'}
+        expected = {'category': 'Starter', 'menuitems': [], 'type': 'Food'}
 
         menugroup = MenuGroup(type="Food", category="Starter")
         db.session.add(menugroup)
         db.session.commit()
         quried_menugroup = db.session.query(MenuGroup).first()
         serialised_menugroup = MenuGroupSchema().dump(quried_menugroup)
+
         assert serialised_menugroup == expected
 
 
 class TestAllergenSchema:
     # An allergen with no menuitem returned by a query is serializable.
     def test_serialise_allergen_no_menuitem(self, db):
-        expected = {'id': 1, 'menuitems': [], 'name': 'Gluten'}
+        expected = {'menuitems': [], 'name': 'Gluten'}
 
         allergen = Allergen(name="Gluten")
         db.session.add(allergen)
         db.session.commit()
         quiried_allergen = db.session.query(Allergen).first()
         serialised_allergen = AllergenSchema().dump(quiried_allergen)
+
         assert serialised_allergen == expected
+
+
+class TestMenuItemSchema:
+    # A menuitem with no allergen returned by a query is serializable.
+    def test_serialise_menuitem(self, db):
+        expected = {'menugroup': {'type': 'Food', 'category': 'Starter'}, 'allergens': [],
+                    'name': 'Tacos', 'description': 'Crispy tacos filled with cheese',
+                    'calorie': 600, 'price': Decimal('5.00')}
+
+        menugroup = MenuGroup(type="Food", category="Starter")
+        menuitem = MenuItem(name="Tacos", description="Crispy tacos filled with cheese",
+                            calorie=600, price=5.00, menugroup=menugroup)
+        db.session.add(menuitem)
+        db.session.add(menugroup)
+        db.session.commit()
+        quired_menuitem = db.session.query(MenuItem).first()
+        serialised_menuitem = MenuItemSchema().dump(quired_menuitem)
+
+        assert serialised_menuitem == expected
