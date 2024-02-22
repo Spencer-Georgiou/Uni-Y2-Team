2454a2af8337f4a3ef0508f891ce0a24148efdcd
Abigail <Abigail.Hunter-Blanco.2022@live.rhul.ac.uk>
2024-02-22 17:21:03 +0000

    Added sign in and sign up buttons.
    Buttons link to respective web pages and are highlighted when hovered over or clicked. Changed every page to use a global NavBar. Need to update to take note of active pages.

--- app/ui/src/components/NavBar.jsx
+++ app/ui/src/components/NavBar.jsx
@@ -1,17 +1,12 @@
 'use client';
 
-import { Navbar, Dropdown } from 'flowbite-react';
+import { Navbar, Dropdown, Button } from 'flowbite-react';
 import { Link } from 'react-router-dom';
 
 {/* Navigation bar component that allows users to easily select, and be taken to, pages they wish to view */}
 function NavBar() {
     return (
         <Navbar className="bg-cherry">
-
-            {/* Displays restaurant logo */}
-            <Link to="/">
-                <img className="h-16" src="/images/OaxacaLogo.png" alt="Oaxaca Logo" />
-            </Link>
 
             {/* Any items within collapse tags will 'disappear' on smaller windows */}
             <Navbar.Collapse>
@@ -41,13 +36,20 @@ function NavBar() {
                 <Link to="/Contact" className="font-sans font-medium font-semibold text-lemon hover:text-amber hover:underline">
                     <span>Contact Us</span>
                 </Link>
+
             </Navbar.Collapse>
 
-            <div className="flex md:order-2">
+            <div className="flex md:order-1">
                 {/* Displays restaurant logo */}
                 <Link to="/">
                     <img className="h-16" src="/images/OaxacaLogo.png" alt="Oaxaca Logo" />
                 </Link>
+            </div>
+
+            <div className="flex gap-2 md:order-2">
+                <Button className="font-sans font-semibold text-cherry bg-lemon hover:ring-4 hover:ring-amber">Log In</Button>
+                <Button className="font-sans font-semibold text-cherry bg-lemon hover:ring-4 hover:ring-amber">Sign Up</Button>
+                {/* Page navigation options disappear into hamburger dropdown button on smaller screens */}
                 <Navbar.Toggle className="text-cherry bg-lemon ring-lemon focus:ring-amber hover:bg-amber" />
             </div>
         </Navbar>
