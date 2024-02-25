4676f66c6356aa068ab2d4cb4e1292a9e13c3f81
Zubiya Malek <zjap079@live.rhul.ac.uk>
2024-02-25 12:10:29 +0000

    Completed "About us" page with icons for each team member that blur and display text. Opacity of certain elements has been corrected.

--- app/ui/src/Pages/aboutUs/AboutUs.jsx
+++ app/ui/src/Pages/aboutUs/AboutUs.jsx
@@ -4,62 +4,66 @@ const AboutUs = () => {
         <div>
             <div
                 className="w-full h-screen bg-no-repeat bg-cover bg-center flex justify-center items-center relative"
-                style={{ backgroundImage: "url('/Homepage/Taco.jpg')" }}>
+                style={{ backgroundImage: "url('/aboutUs/Building.jpg')" }}>
 
                 <div className="flex flex-row space-x-4">
+
                     
 
-                    <div className="bg-lemon h-[1000px] w-[200px] bg-lemon rounded-[25px] m-auto opacity-95">
-                        <div className="opacity-100 pl-1 space-y-10 w-25 pt-10 rounded-full">
-                            <div className="rounded-full relative duration-300 rounded-lg hover:hue-rotate-180">
-                                <img src="/aboutUs/Strawberry.png" />
+                    <div className="bg-lemon h-[1000px] w-[200px] bg-lemon rounded-[25px] m-auto bg-opacity-85">
+                        <div className="pl-1 space-y-10 w-25 pt-10">
+                            <div className="relative block group">
+                                <img className="group-hover:blur duration-300 opacity-80" src="/aboutUs/Guacamole.png" />
 
                                 <div className="opacity-0 hover:opacity-100 duration-300 absolute 
-                                inset-0 z-10 flex text-center items-center tracking-wider leading-normal text-3xl 
-                                text-white font-sans">Abigail<br/> Restaurant Manager</div>
-                          
+                                inset-0 z-10 flex text-center justify-center items-center tracking-widest leading-normal text-3xl 
+                                text-white font-sans font-semibold">Abigail<br/>Restaurant Manager</div>
+                           
                             </div>
 
 
-                            <div className="rounded-full relative duration-300 rounded-lg hover:hue-rotate-180">
-                                <img src="/aboutUs/Pineapple.png" />
-
-                                <div className="opacity-0 hover:opacity-100 duration-300 absolute 
-                                inset-0 z-10 flex text-center justify-center items-center tracking-wider leading-normal text-3xl 
-                                text-white font-sans">Natasha<br/> Head <br/> Chef</div>
-
-                            </div>
-
-                            <div className="rounded-full relative duration-300 rounded-lg hover:hue-rotate-180">
-                                <img src="/aboutUs/Watermelon.png" />
+                            <div className="relative block group">
+                                <img className="group-hover:blur duration-300 opacity-80" src="/aboutUs/HotSauce.png" />
 
                                 <div className="opacity-0 hover:opacity-100 duration-300 absolute 
                                 inset-0 z-10 flex text-center justify-center items-center tracking-wider leading-normal text-3xl 
-                                text-white font-sans">Nicky<br/> Social Media Consultant</div>
-                            
+                                text-white font-sans font-bold">Natasha<br/>Executive Chef</div>
+                           
                             </div>
 
-                                <img className="transition-all duration-300 rounded-lg blur-none hover:blur" src="/aboutUs/Lemon.png" />
-                                {/* <p className="opacity-0 hover:opacity-100 duration-300 absolute 
-                                inset-0 z-10 flex text-center items-center tracking-wider leading-normal text-3xl 
-                                text-white font-sans">Abigail<br/> Restaurant Manager</p> */}
+                            <div className="relative block group">
+                                <img className="group-hover:blur duration-300 opacity-80" src="/aboutUs/Taco.png" />
 
+                                <div className="opacity-0 hover:opacity-100 duration-300 absolute 
+                                inset-0 z-10 flex text-center justify-center items-center leading-normal text-3xl 
+                                text-white font-sans font-bold">Nicky<br/> Social Media Specialist</div>
+                           
+                            </div>
+
+                            <div className="relative block group">
+                                <img className="group-hover:blur duration-300 opacity-80" src="/aboutUs/Popsicle.png" />
+
+                                <div className="opacity-0 hover:opacity-100 duration-300 absolute 
+                                inset-0 z-10 flex text-center justify-center items-center tracking-wider leading-normal text-3xl 
+                                text-white font-sans font-bold">Zubiya <br/> Food Scientist</div>
+                           
+                            </div>
                         </div>
                     </div>
 
 
-                    <div className="h-[1000px] w-[1200px] bg-lemon rounded-[25px] py-5 m-auto">
+                    <div className="h-[1000px] w-[1200px] bg-lemon rounded-[25px] py-5 m-auto opacity-95">
                         <div className="flex justify-center ">
                             <img src="/aboutUs/Team27.png"/>
 
                         </div>
 
-                        <p class="mr-10 ml-10 font-sans text-xl font-semibold text-center tracking-wider leading-loose"> <br/> Welcome to Oaxaca, 
+                        <p class="mr-10 ml-10 font-sans text-xl font-semibold text-center tracking-widest leading-loose"> <br/> Welcome to Oaxaca, 
                         where passion for Mexican cuisine meets a commitment to 
                         sharing the vibrant flavours of Mexico with our community. 
                         Our journey began with a love affair with Mexican food that 
                         transcended borders and ignited a desire to bring the essence of Mexico to your table.
-                        Founded by Team 27, our story is one of passion, flavour, and tradition.  <br/><br/>
+                        Founded by Team 27- our story is one of passion, flavour, and tradition.  <br/><br/>
                         
                         At Oaxaca, we believe that food has the power to transcend boundaries, 
                         connect people, and create lasting memories. 
@@ -79,29 +83,55 @@ const AboutUs = () => {
                         where every bite tells a story and every meal is a celebration of the vibrant 
                         flavours and rich traditions that make Mexican cuisine truly unforgettable.</p>
 
+       
                     </div>
 
 
-                    <div className="bg-lemon h-[1000px] w-[200px] bg-lemon rounded-[25px] py-0 m-auto opacity-95">
-                        <div className="opacity-100 w-25 rounded-full">
-                            <div className="transition-all duration-300 rounded-lg blur-none hover:blur">
-                                <img src="/aboutUs/Lemon.png" />
+                    <div className="bg-lemon h-[1000px] w-[200px] bg-lemon rounded-[25px] bg-opacity-85">
+                        <div>
+                            <div className="relative block group">
+                                <img className="group-hover:blur duration-300 opacity-80" src="/aboutUs/Taco.png" />
+
+                                <div className="opacity-0 hover:opacity-100 duration-300 absolute 
+                                inset-0 z-10 flex text-center justify-center items-center leading-normal text-3xl 
+                                text-white font-sans font-bold">Belal<br/> Finance Manager</div>
+                           
                             </div>
 
-                            <div className="transition-all duration-300 rounded-lg blur-none hover:blur">
-                                <img src="/aboutUs/Orange.png" />
+                            <div className="relative block group">
+                                <img className="group-hover:blur duration-300 opacity-80" src="/aboutUs/Popsicle.png" />
+
+                                <div className="opacity-0 hover:opacity-100 duration-300 absolute 
+                                inset-0 z-10 flex text-center justify-center items-center leading-normal text-3xl 
+                                text-white font-sans font-bold">Humza<br/> Inventory Manager</div>
+                           
                             </div>
 
-                            <div className="transition-all duration-300 rounded-lg blur-none hover:blur">
-                                <img src="/aboutUs/Strawberry.png" />
+                            <div className="relative block group">
+                                <img className="group-hover:blur duration-300 opacity-80" src="/aboutUs/Enchilada.png" />
+
+                                <div className="opacity-0 hover:opacity-100 duration-300 absolute 
+                                inset-0 z-10 flex text-center justify-center items-center leading-normal text-3xl 
+                                text-white font-sans font-bold">Liu<br/>Data Analyst</div>
+                           
                             </div>
 
-                            <div className="transition-all duration-300 rounded-lg blur-none hover:blur">
-                                <img src="/aboutUs/Pineapple.png" />
+                            <div className="relative block group">
+                                <img className="group-hover:blur duration-300 opacity-80" src="/aboutUs/HotSauce.png" />
+
+                                <div className="opacity-0 hover:opacity-100 duration-300 absolute 
+                                inset-0 z-10 flex text-center justify-center items-center leading-normal text-3xl 
+                                text-white font-sans font-bold">Luke<br/> Legal Consultant</div>
+                           
                             </div>
 
-                            <div className="transition-all duration-300 rounded-lg blur-none hover:blur">
-                                <img src="/aboutUs/Watermelon.png" />
+                            <div className="relative block group">
+                                <img className="group-hover:blur duration-300 opacity-80" src="/aboutUs/Guacamole.png" />
+
+                                <div className="opacity-0 hover:opacity-100 duration-300 absolute 
+                                inset-0 z-10 flex text-center justify-center items-center leading-normal text-3xl 
+                                text-white font-sans font-bold">Spencer<br/> CEO</div>
+                           
                             </div>
 
                         </div>
