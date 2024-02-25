86f55b6e0408112583d980c3b00e74ec02ee7cb1
Zubiya Malek <zjap079@live.rhul.ac.uk>
2024-02-25 20:21:07 +0000

    All the divs, images, and the text in the main content is responsive- changes size as the screen is maximized/minimized.

--- app/ui/src/Pages/aboutUs/AboutUs.jsx
+++ app/ui/src/Pages/aboutUs/AboutUs.jsx
@@ -7,11 +7,12 @@ const AboutUs = () => {
                 className="w-full h-screen bg-no-repeat bg-cover bg-center flex justify-center items-center relative"
                 style={{ backgroundImage: "url('/aboutUs/Building.jpg')" }}>
 
-                <div className="flex flex-row space-x-4">
+
+                <div className="flex flex-row space-x-4 m-auto flex justify-center">
 
                     
 
-                    <div className="bg-lemon h-[1000px] w-[200px] bg-lemon rounded-[25px] m-auto bg-opacity-85">
+                    <div className="bg-lemon rounded-[25px] m-auto bg-opacity-85 relative flex justify-center">
                         <div>
 
                             <div className="relative block group">
@@ -63,42 +64,42 @@ const AboutUs = () => {
                     </div>
 
 
-                    <div className="h-[1000px] w-[1200px] bg-lemon rounded-[25px] py-5 m-auto opacity-95">
-                        <div className="flex justify-center ">
+                    <div className="flex flex-col relative justify-center bg-lemon rounded-[25px] py-5 m-auto opacity-95">
+                        <div className="flex relative block group items-center justify-center">
                             <img src="/aboutUs/Team27.png"/>
 
                         </div>
-
-                        <p class="mr-10 ml-10 font-sans text-xl font-semibold text-center tracking-widest leading-loose"> <br/> Welcome to Oaxaca, 
-                        where passion for Mexican cuisine meets a commitment to 
-                        sharing the vibrant flavours of Mexico with our community. 
-                        Our journey began with a love affair with Mexican food that 
-                        transcended borders and ignited a desire to bring the essence of Mexico to your table.
-                        Founded by Team 27- our story is one of passion, flavour, and tradition.  <br/><br/>
-                        
-                        At Oaxaca, we believe that food has the power to transcend boundaries, 
-                        connect people, and create lasting memories. 
-                        That's why we're committed to sourcing the freshest ingredients, 
-                        handcrafting every dish with care, and infusing each bite with the 
-                        warmth and hospitality that epitomize Mexican culture. <br/><br/>
-                        
-                        Whether you're craving the fiery heat of jalapeno poppers, 
-                        the savoury satisfaction of a classic burrito, 
-                        or the irresistible indulgence of freshly made churros, 
-                        our menu offers a tantalizing array of options to satisfy every palate.  <br/><br/>
-                        
-                        But more than just a place to eat, Oaxaca is a gathering place,
-                        a home away from home where friends and family come together to savour 
-                        the simple joys of good food and good company. 
-                        So, come join us on a culinary adventure through Mexico, 
-                        where every bite tells a story and every meal is a celebration of the vibrant 
-                        flavours and rich traditions that make Mexican cuisine truly unforgettable.</p>
-
+                        <div className="flex justify-center relative">
+                            <p class="mr-10 ml-10 font-sans text-xl font-semibold text-center tracking-widest leading-loose"> <br/> Welcome to Oaxaca, 
+                            where passion for Mexican cuisine meets a commitment to 
+                            sharing the vibrant flavours of Mexico with our community. 
+                            Our journey began with a love affair with Mexican food that 
+                            transcended borders and ignited a desire to bring the essence of Mexico to your table.
+                            Founded by Team 27- our story is one of passion, flavour, and tradition.  <br/><br/>
+                            
+                            At Oaxaca, we believe that food has the power to transcend boundaries, 
+                            connect people, and create lasting memories. 
+                            That's why we're committed to sourcing the freshest ingredients, 
+                            handcrafting every dish with care, and infusing each bite with the 
+                            warmth and hospitality that epitomize Mexican culture. <br/><br/>
+                            
+                            Whether you're craving the fiery heat of jalapeno poppers, 
+                            the savoury satisfaction of a classic burrito, 
+                            or the irresistible indulgence of freshly made churros, 
+                            our menu offers a tantalizing array of options to satisfy every palate.  <br/><br/>
+                            
+                            But more than just a place to eat, Oaxaca is a gathering place,
+                            a home away from home where friends and family come together to savour 
+                            the simple joys of good food and good company. 
+                            So, come join us on a culinary adventure through Mexico, 
+                            where every bite tells a story and every meal is a celebration of the vibrant 
+                            flavours and rich traditions that make Mexican cuisine truly unforgettable.</p>
+                        </div>
        
                     </div>
 
 
-                    <div className="bg-lemon h-[1000px] w-[200px] bg-lemon rounded-[25px] bg-opacity-85">
+                    <div className="bg-lemon rounded-[25px] m-auto bg-opacity-85 relative flex justify-center">
                         <div>
                             <div className="relative block group">
                                 <img className="group-hover:blur duration-300 opacity-80" src="/aboutUs/Taco.png" />
