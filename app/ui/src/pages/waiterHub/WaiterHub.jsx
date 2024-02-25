1f54e49ffeb2ae2307a34d844696636a9167016e
Natasha Fotoohi <zlac232@live.rhul.ac.uk>
2024-02-25 17:39:44 +0000

    created the NavBar component for the waiter hub with a dropdown icon to log out, this redirects you to the staff login page. The Navbar also contains the logo for the restaurant and text saying 'Waiter Hub'

--- app/ui/src/pages/waiterHub/WaiterHub.jsx
+++ app/ui/src/pages/waiterHub/WaiterHub.jsx
@@ -1,21 +1,25 @@
 import NewOrderButton from "../../components/waiterHub/NewOrderButton";
 import ModifyCancelOrderButton from "../../components/waiterHub/ModifyCancelOrderButton";
 import ChangeMenuButton from "../../components/waiterHub/ChangeMenuButton";
+import NavBarWaiterHub from "../../components/waiterHub/NavBarWaiterHub";
+
 
 const WaiterHub = () => {
-    return(
-        <div className="w-full h-screen bg-cherry">
-            <div className="flex w-screen space-x-4">
-                <div class="flex-1 bg-lemon rounded-[25px] pl-3">Order</div>
+    return (
+        <div>
+            <div className="w-full h-screen bg-cherry">
+                <div className="flex w-screen space-x-4">
+                    <div class="flex-1 bg-lemon rounded-[25px] pl-3">Order</div>
 
-                <div class="flex-1 bg-lemon rounded-[25px] pl-3">Ready</div>
+                    <div class="flex-1 bg-lemon rounded-[25px] pl-3">Ready</div>
 
-                <div class="flex-1 bg-lemon rounded-[25px] pl-3">Customer Help</div>
+                    <div class="flex-1 bg-lemon rounded-[25px] pl-3">Customer Help</div>
 
+                </div>
+                <NewOrderButton />
+                <ModifyCancelOrderButton />
+                <ChangeMenuButton />
             </div>
-            <NewOrderButton />
-            <ModifyCancelOrderButton />
-            <ChangeMenuButton />
         </div>
     );
 }
