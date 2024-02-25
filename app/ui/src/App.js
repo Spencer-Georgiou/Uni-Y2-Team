77d78c3f6c21234d3d2f9db93fc9118022607ce2
Yat Siu Tseung <yatsiutseung@gmail.com>
2024-02-25 23:52:34 +0000

    rename some components

--- app/ui/src/App.js
+++ app/ui/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './index.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import CustomerLogin from './pages/login/CustomerLogin';
-import StaffLogin from './pages/login/StaffLogin';
-import Registration from './pages/registration/Registration';
-import FoodMenu from './pages/menu/FoodMenu';
-import Order from './pages/order/Order';
+import CustomerLogin from './Pages/login/CustomerLogin';
+import StaffLogin from './Pages/login/StaffLogin';
+import Registration from './Pages/registration/Registration';
+import FoodMenu from './Pages/menu/FoodMenu';
+import Order from './Pages/order/Order';
 import NavBar from './components/NavBar';
 
 
@@ -16,7 +16,7 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={<StaffLogin/>} />
+          <Route path="/" element={<StaffLogin />} />
           <Route path="/CustomerLogin" element={<CustomerLogin />} />
           <Route path="/StaffLogin" element={<StaffLogin />} />
           <Route path="/Registration" element={<Registration />} />
