bce59c8ee12feaf8e5708aa004174d23524766d8
Zubiya Malek <zjap079@live.rhul.ac.uk>
2024-02-26 18:45:44 +0000

    Changed filenames and paths to match correct path.

--- app/ui/src/App.js
+++ app/ui/src/App.js
@@ -8,7 +8,7 @@ import FoodMenu from './pages/menu/FoodMenu';
 import DrinkMenu from './pages/menu/DrinkMenu';
 import NavBar from './components/NavBar'
 import Foot from './components/Foot'
-import Homepage from './pages/homepage/Homepage'
+import Homepage from './pages/Homepage/Homepage'
 import AboutUs from './pages/aboutUs/AboutUs'
 
 
