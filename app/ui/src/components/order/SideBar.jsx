29933a2336db92e22c42f4e7525ef8d7eb2c1444
Yat Siu Tseung <yatsiutseung@gmail.com>
2024-02-26 19:35:46 +0000

    setted the layout of inner block and check out button in the order side bar

--- app/ui/src/components/order/SideBar.jsx
+++ app/ui/src/components/order/SideBar.jsx
@@ -1,5 +1,19 @@
+import { Fragment } from "react";
+
 const SideBar = () => {
-  return <div class="mt-24 bg-lemon w-96 h-96">hi</div>;
+  return (
+    <Fragment>
+      <div class="mt-24 bg-lemon w-10/12 h-3/4 ml-8 rounded-3xl">
+        <div>hi , Abi</div>
+      </div>
+      <button
+        type="button"
+        class="w-48 text-lemon bg-cherry rounded-full text-2xl px-5 py-2.5 text-cente mx-36 mt-5"
+      >
+        <b>Check Out</b>
+      </button>
+    </Fragment>
+  );
 };
 
 export default SideBar;
