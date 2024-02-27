21efaf5352bac8b8b62cc8f0ab09ddc7dc72956d
Yat Siu Tseung <yatsiutseung@gmail.com>
2024-02-27 12:41:21 +0000

    completed the starters menu

--- app/ui/src/components/order/Starters.jsx
+++ app/ui/src/components/order/Starters.jsx
@@ -4,7 +4,15 @@ const Starters = () => {
       title: "Tacos",
       name: "Crispy tacos filled with cheese",
       kcal: "600 kcal",
-      price: "$5.00",
+      price: "￡5.00",
+      value: "spicy",
+    },
+    {
+      title: "Patatas Bravas",
+      name: "Roasted potatoes in tomato dressing (V)",
+      kcal: "500 kcal",
+      price: "$3.00",
+      value: "vegetarian",
     },
   ];
 
@@ -13,7 +21,15 @@ const Starters = () => {
       title: "Jalapeno Poppers",
       name: "With cream cheese",
       kcal: "450 kcal",
-      price: "$3.00",
+      price: "￡3.00",
+      value: "spicy",
+    },
+    {
+      title: "Crispy Cauliflower Bites",
+      name: "Roasted cauliflower in jalapeno dressing (V)",
+      kcal: "200 kcal",
+      price: "￡2.50",
+      value: "spicy",
     },
   ];
 
@@ -27,13 +43,13 @@ const Starters = () => {
       <div>
         <div class="w-1/2 float-left">
           {starter1.map((s) => (
-            <div class="w-3/4 bg-amber h-[160px] ml-[100px] my-5 p-3 text-xl">
+            <div class="w-3/4 bg-amber h-[180px] ml-[100px] my-5 p-3 text-xl">
               <p class="text-lemon">
                 <b>{s.title}</b>
               </p>
               <p>• {s.name}</p>
-              <p>{s.kcal}</p>
-              <p class="pt-2">
+              <p class="text-lg">{s.kcal}</p>
+              <p class="pt-1">
                 <b>{s.price}</b>
                 <button
                   type="button"
@@ -47,13 +63,13 @@ const Starters = () => {
         </div>
         <div class="w-1/2 float-right">
           {starter2.map((s) => (
-            <div class="w-3/4 bg-amber h-[160px] ml-[20px] my-5 p-3 text-xl">
+            <div class="w-3/4 bg-amber h-[180px] ml-[20px] my-5 p-3 text-xl">
               <p class="text-lemon">
                 <b>{s.title}</b>
               </p>
               <p>• {s.name}</p>
-              <p>{s.kcal}</p>
-              <p class="pt-2">
+              <p class="text-lg">{s.kcal}</p>
+              <p class="pt-1">
                 <b>{s.price}</b>
                 <button
                   type="button"
