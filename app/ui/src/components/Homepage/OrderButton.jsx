9e0b10a48721e45e5031de98ab783da4f2812fba
Zubiya Malek <zjap079@live.rhul.ac.uk>
2024-02-29 15:24:20 +0000

    Updated Order now button formatting to match orginal design.

--- app/ui/src/components/Homepage/OrderButton.jsx
+++ app/ui/src/components/Homepage/OrderButton.jsx
@@ -5,12 +5,13 @@ import { Link } from 'react-router-dom';
 
 function OrderButton() {
     return (
-        <Link to="/Order" className="flex items-center justify-center">
-        <button type="button" className="bg-amber text-lemon font-sans font-bold py-2 px-4 rounded-lg">
-          Order Now!
-        </button>
-      </Link>
+        <Link to="/Order" className="container py-5 min-w-full grid place-items-center bg-lemon">
+            <button type="button" className="px-12 py-7 text-3xl text-lemon bg-amber rounded-lg text-center font-semibold font-sans">
+                Order Now!
+            </button>
+        </Link>
     );
 }
 
 export default OrderButton
+
