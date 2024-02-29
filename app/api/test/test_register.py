1675b2438791502fe73036d9165f7a3050d5678a
Luke Myers <Luke.Myers.2022@live.rhul.ac.uk>
2024-02-29 18:21:50 +0000

    Fix a bug in test_register where the get request would not send any arguments. Change test_register to test both cases of registration: a user with the same username already exists (registration failure), does not yet exist (registration success).

--- app/api/test/test_register.py
+++ app/api/test/test_register.py
@@ -5,15 +5,18 @@ from src.model import User
 
 
 class TestRegister:
-    # Test if the get response to the menu api returns a list of menu items.
+    # Test if the get response to the register API returns correct message and status code for each condition
     def test_get(self, client, db):
         user = User(username="user", password="pass")
         db.session.add_all([user])
         db.session.commit()
 
-        response = client.get("/api/register")
+        response = client.get("/api/register?username=user&password=pass")
+        assert response.status_code == 403
+        assert response == "Failed"
+
+        response = client.get("/api/register?username=user2&password=pass2")
         assert response.status_code == 200
         assert response == "Success"
-
         all_users = db.session.query(User).all()
         assert user in all_users
