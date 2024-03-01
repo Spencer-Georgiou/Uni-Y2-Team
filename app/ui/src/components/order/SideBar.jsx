d150966f0ce7d05164c61be205d3f9854ccaf777
Yat Siu Tseung <yatsiutseung@gmail.com>
2024-03-01 09:04:29 +0000

    edit the order sidebar

--- app/ui/src/components/order/SideBar.jsx
+++ app/ui/src/components/order/SideBar.jsx
@@ -3,11 +3,15 @@ import { Fragment } from "react";
 const SideBar = ({ order }) => {
   return (
     <Fragment>
-      <div class="mt-24 bg-lemon w-10/12 h-3/4 ml-8 rounded-3xl">
+      <div class="mt-24 bg-lemon w-10/12 h-3/4 ml-8 rounded-3xl p-6">
         {order.length > 0 ? (
           order.map((o, index) => (
             <Fragment key={index}>
-              <p>{o.id}</p>
+              <p class="text-lg">{o.name}</p>
+
+              <p>
+                <b class="mr-[20px]">{o.price} </b> <b>+ {o.amount} -</b>
+              </p>
             </Fragment>
           ))
         ) : (
