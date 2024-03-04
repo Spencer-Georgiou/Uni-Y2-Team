97013fd0f4fedebc77240f59ba31fb110010f539
Natasha Fotoohi <zlac232@live.rhul.ac.uk>
2024-03-04 22:29:17 +0000

    changed the image names to be the resized ones

--- app/ui/src/pages/homepage/Homepage.jsx
+++ app/ui/src/pages/homepage/Homepage.jsx
@@ -7,13 +7,13 @@ const homepage = () => {
       <div>
         <img
           class="h-auto w-full bg-center items-center  "
-          src="/homepage/HomeBannerBackground.png"
+          src="/homepage/HomeBannerBackground_2048.jpg"
         ></img>
       </div>
       <div>
         <img
           class="h-auto w-full bg-center items-center  "
-          src="/homepage/Slogan.png"
+          src="/homepage/Slogan_without_shadow.jpg"
         ></img>
         <img
           class="h-auto w-full bg-center items-center  "
