dcf3030fdedad6f0d87702fdf7f75b9e1f4e8f3d
Liu Zhang <Liu.ZHANG.2023@live.rhul.ac.uk>
2024-03-07 23:20:02 +0000

    feat(Session): two sessions cannot associate with the same user

--- app/api/test/test_models/test_session.py
+++ app/api/test/test_models/test_session.py
@@ -1,3 +1,8 @@
+from src.models import Session
+from sqlalchemy.exc import IntegrityError
+import pytest
+
+
 class TestSession:
     # An instance of a session object can be created and stored in the database.
     def test_create_session(self, db, session_for_waiter):
@@ -10,3 +15,15 @@ class TestSession:
         db.session.commit()
 
         assert session_for_waiter.user is waiter
+
+    # Two sessions cannot associate with the same user.
+    def test_avoid_relationship_same_user(self, db, session_for_waiter):
+        # when an session in the database
+        db.session.add(session_for_waiter)
+        db.session.commit()
+
+        # # create another session pointing to the same user should raise an error
+        with pytest.raises(IntegrityError):
+            another_session = Session(user_username=session_for_waiter.user_username, token="abcde")
+            db.session.add(another_session)
+            db.session.commit()
