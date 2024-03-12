13d9b240dbbc10c0e1875a67878ae59287c7bc67
Abigail <Abigail.Hunter-Blanco.2022@live.rhul.ac.uk>
2024-03-12 22:34:38 +0000

    Changed Sign Up form to have same design as the login pages
    Changed the registration sign up form to use the same sizes and fonts as the login sign in forms.

--- app/ui/src/components/registration/SignUpForm.jsx
+++ app/ui/src/components/registration/SignUpForm.jsx
@@ -3,31 +3,45 @@ import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
 {/*Form that allows new customers to register an account*/}
 const SignUpForm = () => {
   return (
-    <div>
-                <img className="w-40 h-40 m-auto mb-2" src="/images/OaxacaLogo.png" alt="Oaxaca Logo" />
-                    <div class="flex justify-center">
-
-                    <form className="flex max-w-md flex-col gap-4">
-                        <div className="text-2xl font-sans font-semibold text-center">Customer Sign Up</div>
-                        <div>
-                            <div className="mb-2 block">
-                            <Label htmlFor="email1" value="Your email" />
-                            </div>
-                            <TextInput id="email1" type="email" placeholder="Email" required />
-                        </div>
-                        <div>
-                            <div className="mb-2 block">
-                            <Label htmlFor="password1" value="Your password" />
-                            </div>
-                            <TextInput id="password1" type="password" placeholder="Password" required />
-                        </div>
-                        <div className="flex justify-center">
-                          <Button type="submit" className="h-16 font-sans font-semibold text-cherry bg-lemon rounded-lg text-xl w-40 rounded-2xl px-5 py-2.5 text-center">Sign Up</Button>
-                        </div>
-                        </form>
-    
-                    </div>
-                </div>
+    <form className="px-6 mx-auto">
+    {/*Input field that allows users to input their usernames*/}
+    <div className="mb-5">
+      <b>
+        {/*When user submits form, assign input to username variable*/}
+        <input
+          type="text"
+          id="email"
+          name="email"
+          className=" text-xl text-black font-semibold text-center h-14 bg-lemon border border-lemon rounded-2xl block w-full p-2.5"
+          placeholder="Email"
+          required
+        />
+      </b>
+    </div>
+    {/*Input field that allows users to input their password*/}
+    <div className="mb-5 ">
+      <b>
+        {/*When user submits form, assign input to username variable*/}
+        <input
+          type="text"
+          id="password"
+          name="password"
+          className="text-xl text-black font-semibold text-center h-14 bg-lemon border border-lemon rounded-2xl block w-full p-2.5"
+          placeholder="Password"
+          required
+        />
+      </b>
+    </div>
+    {/*Button to submit form*/}
+    <div className="flex justify-center">
+      <button
+        type="submit"
+        className="h-16 font-sans font-semibold bg-lemon rounded-lg text-xl w-40 rounded-2xl px-5 py-2.5 text-center"
+      >
+        Sign Up
+      </button>
+    </div>
+  </form>
   );
 }
 
