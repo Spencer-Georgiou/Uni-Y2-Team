6f3974a4704fd16e8e341d8724fbb2dec5baaf0e
Natasha Fotoohi <zlac232@live.rhul.ac.uk>
2024-03-13 22:47:25 +0000

    a button is now displayed next to each order in the kitchen hub and display orders has been modified to display the button only in the kitchen hub

--- app/ui/src/Pages/kitchenHub/KitchenHub.jsx
+++ app/ui/src/Pages/kitchenHub/KitchenHub.jsx
@@ -1,11 +1,18 @@
+import ReadyAndOrderDisplay from "../../components/kitchenHub/ReadyAndOrderDisplay";
+import DisplayOrders from "../../components/waiterHub/DisplayOrders";
+import ReadyButton from "../../components/kitchenHub/ReadyButton";
+
+
+
 const KitchenHub = () => {
     return (
         <div className="flex flex-col space-y-16 w-full h-screen bg-redder justify-center items-center ">
             <div className="flex h-5/6 w-5/6 bg-lemon flex-col space-y-1 mt-5 text-xl text-redder font-semibold">
-            <div className="ml-4">Confirmed Orders</div>
-            <div className="h-1 bg-redder"></div>
-
-
+                <div className="ml-4">Confirmed Orders</div>
+                <div className="h-1 bg-redder"></div>
+                <div className="flex">
+                    <DisplayOrders readyButton={true} />
+                </div>
             </div>
         </div>
 
---
+++ app/ui/src/components/kitchenHub/ReadyButton.jsx
@@ -0,0 +1,10 @@
+'use client';
+
+import { Button } from 'flowbite-react';
+
+function ReadyButton() {
+    return (
+        <Button color="success">Success</Button>
+    )
+}
+export default ReadyButton
