daf0d5db2d1c515222d52e7880530a4402145df8
Abigail <Abigail.Hunter-Blanco.2022@live.rhul.ac.uk>
2024-03-13 14:30:14 +0000

    Added email and password storing to sign up form
    Regsitration sign up form now stores user inputs and posts it to the register api

--- app/ui/src/components/registration/SignUpForm.jsx
+++ app/ui/src/components/registration/SignUpForm.jsx
@@ -1,16 +1,56 @@
 import { Button, Checkbox, Label, TextInput } from 'flowbite-react';
+import React, {useState} from "react";
 
 {/*Form that allows new customers to register an account*/}
 const SignUpForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  {/*Change Email to user input*/}
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  {/*Change Password to user input*/}
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault(); {/*prevents the default form from submitting and keeps the page from reloading*/}
+
+    const postingData = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password,
+      }),
+    };
+    fetch("/api/register", postingData)
+      .then((response) => {
+        if (response.status === 200) return response.json();
+        else alert("error here");
+      })
+      .then()
+      .catch((error) => {
+        console.error("there was an error", error);
+      });
+  };
+
   return (
-    <form className="px-6 mx-auto">
-    {/*Input field that allows users to input their usernames*/}
+    <form onSubmit={handleSubmit} className="px-6 mx-auto">
+    {/*Input field that allows users to input their emails*/}
     <div className="mb-5">
       <b>
-        {/*When user submits form, assign input to username variable*/}
+        {/*When user submits form, assign input to email variable*/}
         <input
           type="text"
           id="email"
+          value={email}
+          onChange={handleEmailChange}
           name="email"
           className=" text-xl text-black font-semibold text-center h-14 bg-lemon border border-lemon rounded-2xl block w-full p-2.5"
           placeholder="Email"
@@ -25,6 +65,8 @@ const SignUpForm = () => {
         <input
           type="text"
           id="password"
+          value={password}
+          onChange={handleEmailChange}
           name="password"
           className="text-xl text-black font-semibold text-center h-14 bg-lemon border border-lemon rounded-2xl block w-full p-2.5"
           placeholder="Password"
