fdb09833acf8689213eb7b482309b1da699e2c9f
Luke Myers <Luke.Myers.2022@live.rhul.ac.uk>
2024-03-14 19:04:54 +0000

    Add adhoc ssl_context.

--- app/api/src/api.py
+++ app/api/src/api.py
@@ -8,4 +8,4 @@ from src import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host=app.config["HOST"], port=app.config["PORT"], debug=True)
+    app.run(host=app.config["HOST"], port=app.config["PORT"], debug=True, ssl_context='adhoc')
