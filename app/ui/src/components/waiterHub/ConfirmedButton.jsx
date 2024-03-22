87678c7aa413ca812ffe417f48106208d80d9d7c
Zubiya Malek <zjap079@live.rhul.ac.uk>
2024-03-22 05:35:32 +0000

    The orders which are in "Confirming" state are automatically dislpayed- and removed when state is changed. Will merge release-sprint4 branch into waiter-ui and continue working there.

--- app/ui/src/components/waiterHub/ConfirmedButton.jsx
+++ app/ui/src/components/waiterHub/ConfirmedButton.jsx
@@ -1,14 +1,13 @@
 import { Button } from 'flowbite-react';
 import { useState } from 'react';
 
-function ConfirmedButton({ orderId }) {
+function ConfirmedButton({ orderId, onOrderDelivered }) {
   const [buttonColour, setButtonColour] = useState('green');
   const [textColour, setTextColour] = useState('white');
-  
+
   const handleReady = () => {
     setButtonColour('transparent');
     setTextColour('transparent');
-
 
     const patchData = {
       id: orderId,
@@ -27,23 +26,18 @@ function ConfirmedButton({ orderId }) {
         if (!response.ok) {
           throw new Error('Failed to mark order as ready');
         }
+        onOrderDelivered(orderId);
         // Handle success, maybe display a success message
       })
       .catch(error => {
         console.error('Error marking order as ready:', error);
         // Handle error, display an error message to the user
       });
-
-      // window.location.reload();
   };
 
   return (
-    <Button  style={{ backgroundColor: buttonColour, color: textColour, outline:'transparent' }} onClick={handleReady}>Confirm Order</Button>
+    <Button style={{ backgroundColor: buttonColour, color: textColour, outline: 'transparent' }} onClick={handleReady}>Confirm Order</Button>
   );
 }
 
 export default ConfirmedButton;
-
-
-
-
