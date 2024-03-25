56b38b20b26227031f5ead0307b2f91f997f48d8
Zubiya Malek <zjap079@live.rhul.ac.uk>
2024-03-25 12:21:28 +0000

    Edited the homepage, so that it matches the original design. Edited ModifyCancelOrder so now, only orders in state "Confirming" and "Preparing" can modify their order. Otherwise, the button is disbled.

--- app/ui/src/components/homepage/CarouselHomepage.jsx
+++ app/ui/src/components/homepage/CarouselHomepage.jsx
@@ -5,7 +5,7 @@ import { Carousel } from 'flowbite-react';
 {/*Displays pictures of the restaurant on two carousels.*/}
 function CarouselHomepage() {
   return (
-    <div className="grid h-56 grid-cols-2 gap-2 sm:h-64 xl:h-80 2xl:h-96 bg-lemon py-3 pl-3 pr-3">
+    <div className="grid h-56 grid-cols-2 gap-2 sm:h-64 xl:h-80 2xl:h-96 bg-amber py-3 pl-3 pr-3">
       <Carousel pauseOnHover slideInterval={2000}>
         <img src="/Homepage/Guac_1024.jpg" />
         <img src="/Homepage/Dancers_1024.jpg" />
