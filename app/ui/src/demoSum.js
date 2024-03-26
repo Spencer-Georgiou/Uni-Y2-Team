c82848b081082482d85fe17c342f70d11302d304
Liu Zhang <Liu.ZHANG.2023@live.rhul.ac.uk>
2024-03-26 23:51:17 +0000

    feat(UI): retrieve test files to make the pipeline pass

---
+++ app/ui/src/demoSum.js
@@ -0,0 +1,13 @@
+/**
+ * A function returns the sum of two numbers.
+ *
+ * @param {number} a an addend
+ * @param {number} b an addend
+ * @returns {number} the sum of two addends
+ *
+ */
+function demoSum (a, b) {
+  return a + b
+}
+
+module.exports = demoSum
